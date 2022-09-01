d946aa1b70c534841362b96284af1406238a257d
Sergey Totmyanin <STotmyanin@gmail.com>
2022-09-01 23:58:48 +0300

    Fix setup issues

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="unicaps",
-    version="1.0.0",
+    version="1.0.0.rc1",
     author="Sergey Scat",
-    author_email="STotmyanin@gmail.com",
+    author_email="py.unicaps@gmail.com",
     description="Universal CAPTCHA Solver for humans",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -22,9 +22,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10"
-        "Programming Language :: Python :: 3.11"
     ],
     python_requires='>=3.7',
 )
