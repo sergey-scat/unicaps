136dd730a379b93252ba182a3296370f0cf4fd5b
Sergey Scat <31442538+sergey-scat@users.noreply.github.com>
2023-03-13 20:35:37 +0300

    Bugfixes (#18)
    * Remove the 'Content-type' header
    * Add as_dict() method to BaseCaptchaSolution
    * Fix Content-type issues
    * Minor fixes
    * List all 2captcha/rucaptcha captchas supported
    * Anticaptcha: fixed error that occurs when the cost parameter is missing from the service response
    * Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="unicaps",
-    version="1.2.0",
+    version="1.2.1",
     author="Sergey Scat",
     author_email="py.unicaps@gmail.com",
     description="Universal CAPTCHA Solver for humans",
